ca7e10b87340388171cbb13a3ed73d201d1aea4c
Jayakrishnaa08 <javnd1234@gmail.com>
2025-01-26 16:23:20 +0530

    DoneSomeChangesToNavBar

--- src/landing_page/components/Search.jsx
+++ src/landing_page/components/Search.jsx
@@ -1,9 +1,19 @@
 const Search = () => {
     return (
         <>
-        <p className="text-[18px]">
-            SearchBarHere
-        </p>
+        <div className="SearchBar
+        flex flex-row items-center justify-between
+        border-[1px] w-pull md:w-auto py-2 rounded-full shadow-sm cursor-pointer">
+            <div className="text-sm px-6 font-semibold">
+                Anywhere
+            </div>
+            <div className="text-sm px-6 font-semibold">
+                Any week
+            </div>
+            <div className="text-sm px-6 font-semibold">
+                Add guests
+            </div>
+        </div>
         </>
     );
   };
--- src/landing_page/components/SignInOptions.jsx
+++ src/landing_page/components/SignInOptions.jsx
@@ -1,10 +1,10 @@
 const SignInOptions = () => {
     return (
         <div className="siginoptions flex flex-row gap-2 items-center">
-        <div className="signin font-bold rounded-lg bg-[#2b300e] p-2 px-3 cursor-pointer">
+        <div className="signin font-bold text-sm rounded-lg bg-[#2b300e] p-2 px-3 cursor-pointer">
             Sign In
         </div>
-        <div className="signup font-bold rounded-lg border-[#2b300e] border-1 p-2 px-3 cursor-pointer">
+        <div className="signup font-bold text-sm rounded-lg border-[#2b300e] border-1 p-2 px-3 cursor-pointer">
             Sign Up
         </div>
         </div>
