24c1e08b8ef3b208c36c5a36646138854922df0b
Jayakrishnaa08 <javnd1234@gmail.com>
2025-01-26 16:34:45 +0530

    DoneSomeChangesToNavBar-2

--- src/landing_page/components/Search.jsx
+++ src/landing_page/components/Search.jsx
@@ -1,17 +1,22 @@
+import {BiSearch} from 'react-icons/bi';
 const Search = () => {
     return (
         <>
         <div className="SearchBar
         flex flex-row items-center justify-between
+        px-2
         border-[1px] w-pull md:w-auto py-2 rounded-full shadow-sm cursor-pointer">
-            <div className="text-sm px-6 font-semibold">
+            <div className="text-sm px-4 font-semibold">
                 Anywhere
             </div>
-            <div className="text-sm px-6 font-semibold">
+            <div className="text-sm px-4 font-semibold">
                 Any week
             </div>
-            <div className="text-sm px-6 font-semibold">
+            <div className="text-sm px-4 font-semibold">
                 Add guests
+            </div>
+            <div className='p-2 text-[#e7ffe5] bg-[#2b300e] rounded-full'>
+                <BiSearch size={18}></BiSearch>
             </div>
         </div>
         </>
